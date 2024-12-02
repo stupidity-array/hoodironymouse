4dd694c7d3011789ac16e04ddc662067b362a83f
stupidity-array <119355139+stupidity-array@users.noreply.github.com>
2024-12-02 14:37:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ let blud = document.querySelector("img");
 blud.style.position = "absolute";
 document.addEventListener("mousemove", (e) => {
   blud.style.left = e.clientX;
+  alert(e.clientX);
 });
