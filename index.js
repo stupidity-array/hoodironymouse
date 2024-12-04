e7a1d1b1a856ec44f61cb197d682c87b6df9ef32
stupidity-array <119355139+stupidity-array@users.noreply.github.com>
2024-12-04 13:45:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const blud = document.querySelector("img");
+const blud = document.getElementById("blud");
 blud.style.position = "absolute";
+const sub = document.getElementById("sub");
+sub.style.position = "absolute";
 
 document.addEventListener("mousemove", (e) => {
-  blud.style.left = e.clientX + "px";
-  blud.style.top  = e.clientY + "px";
+  blud.style.left = (e.clientX - 100) + "px";
+  blud.style.top  = (e.clientY - 100) + "px";
 });
+
+setInterval(() => {
+  sub.style.left = (window.innerWidth) - 240 + "px";
+  sub.style.top = (window.innerHeight) - 135 + "px";
+}, 500);
