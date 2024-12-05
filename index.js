aff983dca0e4c7dab72da82e363fd186f09e2b83
stupidity-array <119355139+stupidity-array@users.noreply.github.com>
2024-12-05 14:37:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const blud = document.getElementById("blud");
-blud.style.position = "absolute";
-const sub = document.getElementById("sub");
-sub.style.position = "absolute";
+blud.style.position = "absolute"
 
 document.addEventListener("mousemove", (e) => {
   blud.style.left = (e.clientX - 100) + "px";
@@ -9,8 +7,13 @@ document.addEventListener("mousemove", (e) => {
 });
 
 setInterval(() => {
+  const sub = document.createElement("img");
+  sub.src = "https://media1.tenor.com/images/75502f3c757d3096182ce7aca7f2d01e/tenor.gif?itemid=14347319";
+  sub.style.position = "absolute";
   const xMax = (window.innerWidth) - 240;
   const yMax = (window.innerHeight) - 135
   sub.style.left = Math.floor(Math.random() * xMax) + "px";
   sub.style.top  = Math.floor(Math.random() * yMax) + "px";
-}, 1500);
+  document.body.prepend(sub);
+  setTimeout(() => {sub.remove();}, 4000);
+}, Math.floor(Math.random() * 5001));
