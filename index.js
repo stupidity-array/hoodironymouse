06eb8db3c792526e6214073d8d8315db04868013
stupidity-array <119355139+stupidity-array@users.noreply.github.com>
2024-12-05 15:01:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ document.addEventListener("mousemove", (e) => {
   blud.style.top  = (e.clientY - 100) + "px";
 });
 
-setInterval(() => {
-  const sub = document.createElement("img");
-  sub.src = "https://media1.tenor.com/images/75502f3c757d3096182ce7aca7f2d01e/tenor.gif?itemid=14347319";
-  sub.style.position = "absolute";
-  const xMax = (window.innerWidth) - 240;
-  const yMax = (window.innerHeight) - 135
-  sub.style.left = Math.floor(Math.random() * xMax) + "px";
-  sub.style.top  = Math.floor(Math.random() * yMax) + "px";
-  document.body.prepend(sub);
-  setTimeout(() => {sub.remove();}, 4000);
-}, Math.floor(Math.random() * 5001));
+function randomImageSpam( src, resW, resH, stayTime, intervalMin, intervalMax ) { 
+  setInterval(() => {
+    const spam = document.createElement("img");
+    spam.src = src;
+    spam.style.position = "absolute";
+    const xMax = (window.innerWidth) - Math.floor(resW * 0.5);
+    const yMax = (window.innerHeight) - Math.floor(resH * 0.5);
+    spam.style.left = Math.floor(Math.random() * xMax) + "px";
+    spam.style.top  = Math.floor(Math.random() * yMax) + "px";
+    document.body.prepend(spam);
+    setTimeout(() => {spam.remove();}, stayTime);
+  }, Math.floor(Math.random() * (intervalMax + 1 - intervalMin)));
+}
+
+randomImageSpam("https://media1.tenor.com/images/75502f3c757d3096182ce7aca7f2d01e/tenor.gif?itemid=14347319", 480, 270, 4000, 2000, 5000);
+randomImageSpam("https://cdn.pixabay.com/animation/2023/01/20/02/36/02-36-45-286_512.gif", 512, 512, 3000, 1000, 3000);
