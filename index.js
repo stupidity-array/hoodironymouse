84ffe6e35ff9be326c8260cf2219647b7d8cf4c5
stupidity-array <119355139+stupidity-array@users.noreply.github.com>
2024-12-08 19:24:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const blud = document.getElementById("blud");
-blud.style.position = "absolute"
+blud.style.position = "absolute";
 
 document.addEventListener("mousemove", (e) => {
   blud.style.left = (e.clientX - 100) + "px";
@@ -7,18 +7,21 @@ document.addEventListener("mousemove", (e) => {
 });
 
 function randomImageSpam( src, resW, resH, stayTime, intervalMin, intervalMax ) { 
-  setInterval(() => {
+  setTimeout(() => {
     const spam = document.createElement("img");
     spam.src = src;
     spam.style.position = "absolute";
+    spam.style.width  = resW + "px";
+    spam.style.height = resH + "px";
     const xMax = (window.innerWidth) - Math.floor(resW * 0.5);
     const yMax = (window.innerHeight) - Math.floor(resH * 0.5);
     spam.style.left = Math.floor(Math.random() * xMax) + "px";
     spam.style.top  = Math.floor(Math.random() * yMax) + "px";
     document.body.prepend(spam);
     setTimeout(() => {spam.remove();}, stayTime);
-  }, Math.floor(Math.random() * (intervalMax + 1 - intervalMin)));
+  }, Math.floor(intervalMin + Math.random() * (intervalMax - intervalMin));
+  randomImageSpam( src, resW, resH, stayTime, intervalMin, intervalMax );
 }
 
-randomImageSpam("https://media1.tenor.com/images/75502f3c757d3096182ce7aca7f2d01e/tenor.gif?itemid=14347319", 480, 270, 4000, 2000, 5000);
-randomImageSpam("https://cdn.pixabay.com/animation/2023/01/20/02/36/02-36-45-286_512.gif", 512, 512, 3000, 1000, 3000);
+randomImageSpam("/sub.gif", 480, 270, 4000, 2000, 5000);
+randomImageSpam("https://cdn.pixabay.com/animation/2023/01/20/02/36/02-36-45-286_512.gif", 170, 170, 3000, 1000, 3000);
